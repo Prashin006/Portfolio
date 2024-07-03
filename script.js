11c12593829480a67933a7ead35e870155130977
Prashin Parikh <prashinparikh99@gmail.com>
2024-07-04 00:56:27 +0530

    First commit

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Display a welcome message on page load
+    setTimeout(function() {
+        alert("Welcome to Prashin Parikh's Portfolio!");
+    }, 1000);
+});
